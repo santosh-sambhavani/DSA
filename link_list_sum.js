f3084f95ea11b96cfe99ccfc52bf1453fc6d3589
Santosh1652 <80413971+Santosh1652@users.noreply.github.com>
2022-05-24 16:03:15 +0530

    final commit

--- link_list_sum.js
+++ link_list_sum.js
@@ -5,7 +5,7 @@ class Solution {
         let rev_1 = this.reverse(first);
         let rev_2 = this.reverse(second);
         let sum_head = null;
-        let sum_tail = sum_head;
+        let sum_tail = null;
         let carry = 0;
         let temp = 0;
         
@@ -26,15 +26,20 @@ class Solution {
             }
             
             if(!sum_tail) {
-                sum_tail = new Node(temp % 10);
+                sum_head = new Node(temp % 10);
+                sum_tail = sum_head;
             }
             else {
                 sum_tail.next = new Node(temp % 10);
                 sum_tail = sum_tail.next;
             }    
             
-            carry = temp / 10;
+            carry = Math.floor(temp / 10);
         } 
+        
+        if(carry !== 0) {
+            sum_tail.next = new Node(carry);
+        }
         
         let ans = this.reverse(sum_head);
         
