c32f5d8bfe351a75d60bbb1c81eadfab4b2c9e8f
Santosh-745 <80413971+Santosh-745@users.noreply.github.com>
2023-09-06 20:35:05 +0530

    Update book-allocation.cpp

--- binary-search/book-allocation.cpp
+++ binary-search/book-allocation.cpp
@@ -1,3 +1,9 @@
+/*
+Given that there are N books and M students. Also given are the number of pages in each book in ascending order. 
+The task is to assign books in such a way that the maximum number of pages assigned to a student is minimum, 
+with the condition that every student is assigned to read some consecutive books. Print that minimum number of pages.
+*/
+
 class Solution 
 {
     public:
