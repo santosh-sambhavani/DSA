8f78d01db995e6136081f5b9eaf34eb491c1368b
Santosh-745 <80413971+Santosh-745@users.noreply.github.com>
2023-09-09 12:53:05 +0530

    Update 90-deegre-rotation.cpp

--- 2D-arrays/90-deegre-rotation.cpp
+++ 2D-arrays/90-deegre-rotation.cpp
@@ -33,4 +33,39 @@ public:
             lastCol--;
         }
     }
+
+    void rotateAntiClockwise(vector<vector<int> >& matrix, int n) 
+    { 
+        int firstRow = 0, 
+            firstCol = 0,
+            lastRow = matrix.size() - 1,
+            lastCol = matrix.size() - 1;
+        
+        while(firstCol < lastCol) {
+            int shift = 0;
+            for(int col = lastCol; col > firstCol; col--) {
+                // Left shift
+                int temp1 = matrix[firstRow][firstCol + shift];
+                matrix[firstRow][firstCol + shift] 
+                    = matrix[firstRow + shift][lastCol];
+                
+                // Down shift
+                int temp2 = matrix[lastRow-shift][firstCol];
+                matrix[lastRow-shift][firstCol] = temp1;
+                
+                // Right shift
+                temp1 = matrix[lastRow][lastCol-shift];
+                matrix[lastRow][lastCol-shift] = temp2;
+                
+                // Up shift
+                matrix[firstRow + shift][lastCol] = temp1;
+                
+                shift++;
+            }
+            firstRow++;
+            firstCol++;
+            lastRow--;
+            lastCol--;
+        }
+    } 
 };
