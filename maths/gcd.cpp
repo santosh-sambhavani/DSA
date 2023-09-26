cdec1f18f0740e65a844472dda352d9e179a8d4e
Santosh-745 <80413971+Santosh-745@users.noreply.github.com>
2023-09-26 21:10:54 +0530

    Create gcd.cpp

---
+++ maths/gcd.cpp
@@ -0,0 +1,26 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int gcd(int a, int b) {
+    while(a != 0 && b!= 0) {
+        if(a >= b) {
+            a = a - b;
+        } else {
+            b = b - a;
+        }
+    }
+    if(a == 0)
+        return b;
+    else 
+        return a;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << "GCD of a & b is: " << gcd(a, b);
+}
+
+// } Driver Code Ends
