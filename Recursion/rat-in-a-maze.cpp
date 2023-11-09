0bdb1eb8c037d6268a08e0af30dc700f7d0c7a43
Santosh-745 <80413971+Santosh-745@users.noreply.github.com>
2023-11-09 08:44:38 +0530

    Update rat-in-a-maze.cpp

--- Recursion/rat-in-a-maze.cpp
+++ Recursion/rat-in-a-maze.cpp
@@ -1,17 +1,30 @@
-private: 
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+    private: 
         bool checkPoint (vector<vector<int>> m, int x, int y, int n, vector<vector<int>> visited) {
             return (x == -1 || x == n || y == -1 || y == n || m[x][y] == 0 || visited[x][y] == 1) ? false : true;
         }
-        
+    
+    private: 
         void generatePaths(
-            vector<vector<int>> m, 
+            vector<vector<int>>& m, 
             int rawIndex,
             int colIndex,
             int n, 
             string output, 
-            vector<string> result,
+            vector<string>& result,
             vector<vector<int>> &visited
         ) {
+            // cout << "\n ===>" << rawIndex << " || " << colIndex;
             if (rawIndex == n-1 && colIndex == n-1) {
                 result.push_back(output);
                 return;
@@ -20,7 +33,7 @@ private:
             // check down
             if (checkPoint(m, rawIndex + 1, colIndex, n, visited)) {
                 // go down
-                rawIndex++;
+                rawIndex += 1;
                 visited[rawIndex][colIndex] = 1;
                 output += 'D';
                 generatePaths(m, rawIndex, colIndex, n, output, result, visited);
@@ -34,7 +47,7 @@ private:
             // check up
             if (checkPoint(m, rawIndex - 1, colIndex, n, visited)) {
                 // go up
-                rawIndex--;
+                rawIndex -= 1;
                 visited[rawIndex][colIndex] = 1;
                 output += 'U';
                 generatePaths(m, rawIndex, colIndex, n, output, result, visited);
@@ -48,7 +61,7 @@ private:
             // check right 
             if (checkPoint(m, rawIndex, colIndex + 1, n, visited)) {
                 // go right
-                colIndex++;
+                colIndex += 1;
                 visited[rawIndex][colIndex] = 1;
                 output += 'R';
                 generatePaths(m, rawIndex, colIndex, n, output, result, visited);
@@ -62,7 +75,7 @@ private:
             // check left
             if (checkPoint(m, rawIndex, colIndex - 1, n, visited)) {
                 // go left
-                colIndex--;
+                colIndex -= 1;
                 visited[rawIndex][colIndex] = 1;
                 output += 'L';
                 generatePaths(m, rawIndex, colIndex, n, output, result, visited);
@@ -74,15 +87,43 @@ private:
             }
         }
     public:
-    vector<string> findPath(vector<vector<int>> &m, int n) {
-        if (m[0][0] == 0 || m[n-1][n-1] == 0)
-            return {};
+    vector<string> findPath(vector<vector<int>>& m, int n) {
         vector<string> result;
-        vector<vector<int>> visited {
-            {1, 0, 0, 0},
-            {0, 0, 0, 0},
-            {0, 0, 0, 0},
-            {0, 0, 0, 0}
-        };
+        if (m[0][0] == 0 || m[n-1][n-1] == 0)
+            return result;
+        vector<vector<int>> visited(n, vector<int>(n, 0));
+        visited[0][0] = 1;
         generatePaths(m, 0, 0, n, "", result, visited);
+        return result;
     }
+};
+
+    
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        sort(result.begin(), result.end());
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
