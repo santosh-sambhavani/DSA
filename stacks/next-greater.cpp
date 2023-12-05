ba2ceef12046a11c4e5ed536af407a5fb1518866
Santosh-745 <80413971+Santosh-745@users.noreply.github.com>
2023-12-05 23:20:22 +0530

    Create next-greater.cpp

---
+++ stacks/next-greater.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User Function Template for C++ solution
+
+class Solution {
+  public:
+    vector<long long> nextLargerElement(vector<long long> &arr, int n){
+        vector<long long> ans(n);
+        stack<long long> s;
+        
+        for(int i=n-1; i >= 0; i--) {
+            while(!s.empty() && s.top() <= arr[i]) {
+                s.pop();
+            }
+            
+            ans[i] = s.empty() ? -1 : s.top();
+            s.push(arr[i]);
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{   
+  int n;
+  cin>>n;
+  vector<long long> arr(n);
+  for(int i=0;i<n;i++)
+      cin>>arr[i];
+  
+  Solution ob;
+  
+  vector <long long> res = ob.nextLargerElement(arr, n);
+  for (long long i : res) cout << i << " ";
+  cout<<endl;
+	return 0;
+}
+
+// } Driver Code Ends
