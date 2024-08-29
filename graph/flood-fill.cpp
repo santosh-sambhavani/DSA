1ff55dba8e8e4159c256a1fbd5c284daeb94b45b
Santosh-745 <80413971+Santosh-745@users.noreply.github.com>
2024-08-30 01:28:52 +0530

    Update flood-fill.cpp

--- graph/flood-fill.cpp
+++ graph/flood-fill.cpp
@@ -1,68 +1,74 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
         queue<pair<int, int>> q;
-        int color1 = image[sr][sc], m = image.size(), n = image[0].size();
-        // vector<vector<int>> res = image;
+        int oldColor = image[sr][sc], m = image.size(), n = image[0].size();
+        vector<vector<int>> res(m, vector<int>(n, 0));
 
-        q.push({sr, sc});
-        image[sr][sc] = color;
-        cout << "\n sr || sc: " << sr << " || " << sc;
-        // while(!q.empty()) {
-        //     pair<int, int> front = q.front();
-        //     cout << "\n ====> fornt: " 
-        //          << front.first << " || " 
-        //          << front.second 
-        //          << " || color: " << image[front.first][front.second];
-        //     vector<pair<int, int>> dir = {{0,1},{1,0},{0,-1},{-1,0}};
-        //     for (auto &it: dir) {
-        //         if (
-        //             front.first+it.first >= 0 
-        //             && front.first+it.first < m
-        //             && front.second+it.second >= 0
-        //             && front.second+it.second < n
-        //             && res[front.first+it.first][front.second+it.second] == color1
-        //         ) {
-        //             q.push({front.first+it.first, front.second+it.second});
-        //             // res[front.first+it.first][front.second+it.second] = color;
-        //             cout << "\n ===> iteration pixel: " 
-        //                  << front.first+it.first 
-        //                  << " ||| " 
-        //                  << front.second+it.second
-        //                  << " || color: " << res[front.first+it.first][front.second+it.second];
-        //         }
-        //     }
-        //     q.pop();
-        // }
-        while (!q.empty()) {
-            pair front = q.front();
-            if (front.first + 1 < m &&
-                image[front.first + 1][front.second] == color1
-            ) {
-                q.push({front.first + 1, front.second});
-                image[front.first + 1][front.second] = color;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                res[i][j] = image[i][j];
             }
-            if (front.second + 1 < n &&
-                image[front.first][front.second + 1] == color1
-            ) {
-                q.push({front.first, front.second + 1});
-                image[front.first][front.second + 1] = color;
-            }
-            if (front.first - 1 >= 0 &&
-                image[front.first - 1][front.second] == color1
-            ) {
-                q.push({front.first - 1, front.second});
-                image[front.first - 1][front.second] = color;
-            }
-            if (front.second - 1 >= 0 &&
-                image[front.first][front.second - 1] == color1
-            ) {
-                q.push({front.first, front.second - 1});
-                image[front.first][front.second - 1] = color;
-            }
-            q.pop();
         }
 
+        q.push({sr, sc});
+        res[sr][sc] = newColor;
+        if(image[sr][sc]==newColor)
+            return image;
+        while(!q.empty()) {
+            pair<int, int> front = q.front();            
+            vector<pair<int, int>> dir = {{0,1},{1,0},{0,-1},{-1,0}};
+            for (auto &it: dir) {
+                int newX = front.first+it.first;
+                int newY = front.second+it.second;
+                if (
+                    newX >= 0 
+                    && newX < m
+                    && newY >= 0
+                    && newY < n
+                    && res[newX][newY] == oldColor
+                ) {
+                    q.push({newX, newY});
+                    res[newX][newY] = newColor;
+                }
+            }
+            q.pop();
+        } 
         return res;
     }
 };
+
+//{ Driver Code Starts.
+int main(){
+
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>>image(n, vector<int>(m,0));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++)
+            cin >> image[i][j];
+    }
+    int sr, sc, newColor;
+    cin >> sr >> sc >> newColor;
+    Solution obj;
+    vector<vector<int>> ans = obj.floodFill(image, sr, sc, newColor);
+    for(auto i: ans){
+        for(auto j: i)
+            cout << j << " ";
+        cout << "\n";
+    }
+	return 0;
+}
+// } Driver Code Ends
+/*
+3 3
+1 1 1
+1 1 0
+1 0 1
+1 1 2
+*/
