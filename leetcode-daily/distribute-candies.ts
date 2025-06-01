f574764cad4ec5e687866b9ca7bba902149b12b0
Santosh <80413971+santosh-sambhavani@users.noreply.github.com>
2025-06-01 16:22:16 +0530

    Create distribute-candies.ts

---
+++ leetcode-daily/distribute-candies.ts
@@ -0,0 +1,15 @@
+function distributeCandies(n: number, limit: number): number {
+    let result = 0;
+    for (let i = 0; i <= Math.min(limit, n); i++) {
+        let remaining = n - i;
+
+        if (remaining > 2*limit) continue;
+
+        const maxForSecond = Math.max(0, remaining-limit);
+        const maxForThird = Math.min(remaining, limit);
+
+        result += maxForThird - maxForSecond + 1;
+    }
+
+    return result;
+};
