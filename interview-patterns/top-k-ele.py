c3403510dc02b03f9cc60da1769954526f3f22cf
Santosh <80413971+santosh-sambhavani@users.noreply.github.com>
2026-07-17 22:48:17 +0530

    Update top-k-ele.py

--- interview-patterns/top-k-ele.py
+++ interview-patterns/top-k-ele.py
@@ -64,4 +64,37 @@ class Solution:
                 visited[freq] = True
 
         return response
+# ----------------------------------------------------------------------------------------------------------------------------------------
+# 373. Find K Pairs with Smallest Sums
+# Example 1:
+# Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+# Output: [[1,2],[1,4],[1,6]]
+# Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+
+import heapq
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        min_heap = []
+        heapq.heappush(min_heap, (nums1[0] + nums2[0], 0, 0))
+        response = []
+        len1 = len(nums1)
+        len2 = len(nums2)
+        hashSet = set()
+        hashSet.add((0, 0))
+
+        while k:
+            _, i, j = heapq.heappop(min_heap)
+            response.append([nums1[i], nums2[j]])
+
+            if i+1 < len1 and (i+1, j) not in hashSet:
+                heapq.heappush(min_heap, (nums1[i+1] + nums2[j], i+1, j))
+                hashSet.add((i+1, j))
+            
+            if j+1 < len2 and (i, j+1) not in hashSet:
+                heapq.heappush(min_heap, (nums1[i] + nums2[j+1], i, j+1))
+                hashSet.add((i, j+1))
+
+            k -= 1
+
+        return response
 
