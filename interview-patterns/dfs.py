63c072080b82e7c8275a12386f1c6add4d1bd95e
Santosh <80413971+santosh-sambhavani@users.noreply.github.com>
2026-07-19 14:02:40 +0530

    Update dfs.py

--- interview-patterns/dfs.py
+++ interview-patterns/dfs.py
@@ -39,3 +39,52 @@ class Solution:
                 if self.isCycle(preqMap, alreadyProcessed, course, set(), path): return []
 
         return path
+
+# 200. Number of Islands
+# Example 1:
+# Input: grid = [
+#   ["1","1","1","1","0"],
+#   ["1","1","0","1","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","0","0","0"]
+# ]
+# Output: 1
+# Example 2:
+# Input: grid = [
+#   ["1","1","0","0","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","1","0","0"],
+#   ["0","0","0","1","1"]
+# ]
+# Output: 3
+class Solution:
+    def travelIsland(
+        self, 
+        grid: List[List[str]], 
+        row: int, 
+        col: int, 
+        totalRows: int, 
+        totalCols: int
+    ):
+        grid[row][col] = '0'
+        if row-1 >= 0 and grid[row-1][col] == '1':
+            self.travelIsland(grid, row-1, col, totalRows, totalCols)
+        if row+1 < totalRows and grid[row+1][col] == '1':
+            self.travelIsland(grid, row+1, col, totalRows, totalCols)
+        if col-1 >= 0 and grid[row][col-1] == '1':
+            self.travelIsland(grid, row, col-1, totalRows, totalCols)
+        if col+1 < totalCols and grid[row][col+1] == '1':
+            self.travelIsland(grid, row, col+1, totalRows, totalCols)
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        count = 0
+
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == '1':
+                    count += 1
+                    self.travelIsland(grid, row, col, rows, cols)
+        
+        return count
